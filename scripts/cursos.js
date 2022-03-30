11a770c395950cc56e5f7475591f3d3ed8637329
Jonathan Wanderley <jonathan.wpc@gmail.com>
2022-03-30 14:09:21 -0300

    Modal adicionado na pagina de cursos

---
+++ scripts/cursos.js
@@ -0,0 +1,21 @@
+const cursos = document.querySelectorAll('.item');
+const botoes = document.querySelectorAll('.botao-modal');
+const modal = document.querySelector('.modal');
+const modalClose = modal.querySelector('.close');
+cursos.forEach(curso => {
+    const botao = curso.querySelector('.botao-modal');
+    botao.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let titulo = curso.querySelector('.text h2').innerHTML;
+        modal.querySelector('.text h3').innerHTML = titulo;
+
+        let imagem = curso.querySelector('img').getAttribute('src');
+        modal.querySelector('img').setAttribute('src', imagem);
+
+        modal.style.display = 'flex';
+    });
+});
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
